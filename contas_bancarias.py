1d4e418053c119205d6d2aca37a6af2550268a16
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-02-14 14:41:57 -0300

    Adicionando o método para transferir e getters e setters

--- contas_bancarias.py
+++ contas_bancarias.py
@@ -2,19 +2,40 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print(f'Construindo objeto {self}')
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
 
     def extrato(self):
-        print(f'Saldo de {self.saldo} do titular {self.titular}')
+        print(f'Saldo de {self.__saldo} do titular {self.__titular}')
 
 
     def deposito(self, valor):
-        self.saldo += valor
-        
-        
+        self.__saldo += valor
+
+
     def saque(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+
+    def transferir(self, valor, destino):
+        self.saque(valor)
+        destino.deposito(valor)
+
+
+    def get_saldo(self):
+        return self.__saldo
+
+
+    def get_titular(self):
+        return self.__titular
+
+
+    def get_limite(self):
+        return self.__limite
+
+
+    def set_limite(self, novo_limite):
+        self.__limite = novo_limite
